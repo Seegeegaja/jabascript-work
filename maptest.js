26d6e8d81b1eb4e574cae2ce87cc69f826dc5050
seegeegaja <wnsdyd821@gmail.com>
2025-01-17 17:08:41 +0900

    sda

---
+++ maptest.js
@@ -0,0 +1,13 @@
+let number = [1,2,3,4,5];
+
+number.map((x)=>{
+  console.log(x);
+});
+
+[1,2,3].map((x)=>{
+  console.log(x);
+});
+
+['A' , 'B' ,'C'].map((x, i)=>{
+  console.log(i , ' : ' , x);
+})
